e4f2f0b2c5c57069667a589c4c20d9398fec152e
lianasportscafati <lianasportscafati@gmail.com>
2026-07-27 15:32:09 +0200

    Aggiungi test calcolo percentuale

---
+++ GymFlow-Cloudflare/tests/weight.test.ts
@@ -0,0 +1,13 @@
+import assert from "node:assert/strict";
+import test from "node:test";
+import { calculateWeight, parseWeight } from "../lib/weight";
+
+test("calcola correttamente una percentuale con virgola decimale", () => {
+  assert.equal(parseWeight("67,5"), 67.5);
+  assert.equal(calculateWeight("67,5", 50), "33,75 kg");
+});
+
+test("non produce un carico senza entrambi i valori", () => {
+  assert.equal(calculateWeight("", 50), "");
+  assert.equal(calculateWeight("67,5", null), "");
+});
